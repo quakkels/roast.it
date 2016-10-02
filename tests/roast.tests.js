d976743440de016ee577dc60aa60dbbc450631a8
quakkels <quakkels@gmail.com>
2016-10-02 14:47:57 -0500

    Bootstrapped testing

---
+++ tests/roast.tests.js
@@ -0,0 +1,46 @@
+var roast = require("../roast.it.js");
+
+var TestRoast = require('../roast.js');
+
+roast.it(
+    "Has no tests to start",
+    function canAddATest() {
+        // arrange
+        var testRoast = new TestRoast();
+
+        // assert
+        return 0 === testRoast.tests.length;
+    }
+);
+
+roast.it(
+    "Can add a test",
+    function canAddATest() {
+        // arrange
+        var testRoast = new TestRoast();
+
+        // act
+        testRoast.it('hello', function(){return;});
+
+        // assert
+        return 1 === testRoast.tests.length;
+    }
+);
+
+roast.it(
+    "Can run tests",
+    function canRunTests() {
+        // arrange
+        var testRoast = new TestRoast();
+        testRoast.tests = [{
+            description:"description",
+            testFunction:function(){return true;}
+        }];
+
+        // act
+        var result = testRoast.run();
+
+        // assert
+        return false === testRoast.hasFailingTests;
+    }
+);
